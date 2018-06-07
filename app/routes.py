cf807459c42c26da916361ac3fdbf5dea87436a7
Pratik Prabhu <prabhupratik27@gmail.com>
2018-06-08 00:53:26 +0530

    Some changes to base

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from flask import render_template, request, flash, redirect, url_for, make_respo
 from . import app
 
 
+
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
